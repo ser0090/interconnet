2b800a0455a150cc7cd19fd45eb08a1c555c6879
sergio <ser.0090@gmail.com>
2020-03-20 14:17:58 -0300

    change style

--- butterfly/desing/allocator.sv
+++ butterfly/desing/allocator.sv
@@ -6,15 +6,20 @@
 
 module allocator
   (
-   input wire        clk,       // chip clock
-   input logic [1:0] this_port, // identifies this output port
-   input logic [3:0] r0,        // top four bits of each input phit
-   input logic [3:0] r1,        // directs shifter to discard upper two bits
-   input logic [3:0] r2,
-   input logic [3:0] r3,
-   output wire [3:0] select,
-   output wire       shift
+   output wire [3:0] o_select,
+   output wire       o_shift,
+
+   input logic [1:0] i_this_port, // identifies this output port
+   input logic [3:0] i_r0, // top four bits of each input phit
+   input logic [3:0] i_r1, // directs shifter to discard upper two bits
+   input logic [3:0] i_r2,
+   input logic [3:0] i_r3,
+   input wire        i_rst,
+   input wire        i_clk // chip clock
    ) ;
+
+   localparam HEAD    = 3;
+   localparam PAYLOAD = 2;
 
    wire [3:0]        grant;
    wire [3:0]        head;
@@ -27,27 +32,48 @@ module allocator
 
    reg [3:0]         last;
 
-   assign head = {r3[3:2]==3, r2[3:2]==3, r1[3:2]==3, r0[3:2]==3};
+   initial begin
+      last = 0;
+   end
 
-   assign payload = {r3[3:2] == 2,r2[3:2] == 2,r1[3:2] == 2,r0[3:2] == 2};
+   // Decode section
+   assign head    = {i_r3[3:2] == HEAD, i_r2[3:2] == HEAD,
+                     i_r1[3:2] == HEAD, i_r0[3:2] == HEAD};
 
-   assign match = {r3[1:0] == this_port,
-                   r2[1:0] == this_port,
-                   r1[1:0] == this_port,
-                   r0[1:0] == this_port};
+   assign payload = {i_r3[3:2] == PAYLOAD, i_r2[3:2] == PAYLOAD,
+                     i_r1[3:2] == PAYLOAD, i_r0[3:2] == PAYLOAD};
 
-   assign request = head&match ;
+
+   assign match   = {i_r3[1:0] == i_this_port,
+                     i_r2[1:0] == i_this_port,
+                     i_r1[1:0] == i_this_port,
+                     i_r0[1:0] == i_this_port};
+
+   // use of the output port to route the packets starting whit this head phit
+   assign request = head & match;
    assign pass    = {pass[1:0], avail} & ~request[2:0];
 
+   // Arbitrer section
+   // accepts 4 request signasl y generetare 4 grant signals
+   // if output port is available -> avail.
+   // Arbitrer grants port to the first upper input port making request.
    assign grant   = request & {pass, avail} ;
-   assign hold    = last & payload ;
-   assign select  = grant | hold ;
    assign avail   = ~(|hold) ;
-   assign shift   = |grant ;
 
-   always @(posedge clk) begin
-     last <= select ;
+   // Hold section
+   // mantiene la salida para una entrada para toda la duracion del paquete.
+   // si el pkt es un payload last mantiene la misma asignacion input -> output.
+   assign hold    = last & payload ;
+
+   always @(posedge i_clk) begin
+      if(i_rst)
+        last <= 0;
+      else
+        last <= o_select;
    end
+
+   assign o_select  = grant | hold ;
+   assign o_shift   = |grant ;
 
 endmodule // allocator
 
